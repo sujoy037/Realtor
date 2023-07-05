0eefb7d520d7a0da7fb29b4bac94881e6df72468
Sujoy Halsana <sujoy037@gmail.com>
2023-07-05 09:58:33 -0700

    pages create successfully

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react'
 import './App.css'
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import ForgetPassword from './pages/ForgetPassword'
+import Offers from './pages/Offers'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/profile' element={<Profile/>}/>
+          <Route path='/sign-in' element={<Signin/>}/>
+          <Route path='/sign-up' element={<Signup/>}/>
+          <Route path='/forget-password' element={<ForgetPassword/>}/>
+          <Route path='/offers' element={<Offers/>}/>
+        </Routes>
+      </Router>
+    </>
   )
 }
 
